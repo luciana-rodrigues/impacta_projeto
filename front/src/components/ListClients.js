4ab39b7e77502bb4e3bb4a8210197a5648feb3ae
Rafael Bonifácio <rafael_da_silva_boni@dellteam.com>
2023-04-14 13:24:53 -0300

    feature: add list client and router

---
+++ front/src/components/ListClients.js
@@ -0,0 +1,91 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import ClientService from "../client/client.service";
+import { useState, useEffect } from 'react';
+import { set } from 'react-hook-form';
+import { Button, Grid } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+
+function createData(
+    name,
+    calories,
+    fat,
+    carbs,
+    protein,
+) {
+    return { name, calories, fat, carbs, protein };
+}
+
+const getData = async () => {
+    const clients = await ClientService.getAll()
+    return clients.data
+}
+
+export default function DenseTable() {
+    const [rows, setRows] = useState([{}, {}]);
+    useEffect(() => {
+        getData().then(
+            result => setRows(result)
+        )
+    }, []);
+    return (
+        <Paper
+            elevation={20}
+            align="center"
+
+            sx={{
+                padding: 2,
+                margin: 2,
+                maxWidth: 1200,
+                backgroundColor: "rgba(0,0,0,0.05",
+            }}
+        >
+            <Grid container spacing={3} >
+                <Grid item xs={12}>
+                    <TableContainer component={Paper} spacing={2}>
+                        <Table sx={{ minWidth: 650 }} size="small" aria-label="a  table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="right"><b>Nome</b></TableCell>
+                                    <TableCell align="right"><b>Sobrenome</b></TableCell>
+                                    <TableCell align="right"><b>Data Nasc.</b></TableCell>
+                                    <TableCell align="right"><b>Telefone</b></TableCell>
+                                    <TableCell align="right"><b>email</b></TableCell>
+                                    <TableCell align="right"><b>Data IR</b></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row) => (
+                                    <TableRow
+                                        key={row.nome}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {row.nome}
+                                        </TableCell>
+                                        <TableCell align="right">{row.sobrenome}</TableCell>
+                                        <TableCell align="right">{row.data_nasc}</TableCell>
+                                        <TableCell align="right">{row.telefone}</TableCell>
+                                        <TableCell align="right">{row.email}</TableCell>
+                                        <TableCell align="right">{row.data_ir}</TableCell>
+
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+                <Grid item>
+                    <Button component={Link} to={'/add'} variant="contained" color="primary" spacing={2} margin={2}>Adicionar Cliente</Button>
+                </Grid>
+            </Grid>
+        </Paper>
+    );
+}
