9f4ffa17a5f85ee01cb5c44a75d216c4951b0a86
Rafael Bonifácio <rafael_da_silva_boni@dellteam.com>
2023-06-09 21:07:52 -0300

    add env to backend service

--- front/src/client/client.service.js
+++ front/src/client/client.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: process.env.BACKEND || 'http://localhost:8000',
 })
 
 const ClientService = {
